5c7ad8ef6d5afd01e5c102bf0d746bc069fbe33f
monoloco <monolocoGIT@gmail.com>
2013-10-19 03:26:07 +1100

    Added transformer class

--- Electric_Car_Charge_Scheduling_System/src/agents/Initialiser.java
+++ Electric_Car_Charge_Scheduling_System/src/agents/Initialiser.java
@@ -1,3 +1,4 @@
+
 package agents;
 
 import behaviours.Initialise;
@@ -5,29 +6,32 @@ import jade.core.Agent;
 
 public class Initialiser extends Agent {
 
-	private static final long serialVersionUID = 8349492506914849055L;
+    private static final long serialVersionUID = 8349492506914849055L;
 
-	protected void setup() {
+    protected void setup() {
 
-		/*
-		 * Announce operation of this agent.
-		 */
+        /*
+         * Announce operation of this agent.
+         */
 
-		/*
-		 * Deploy Sniffer agent.
-		 */
-		// super.addBehaviour(new Initialise("Sniffer","jade.tools.sniffer.Sniffer",new Object[]{ "*" }));
+        /*
+         * Deploy Sniffer agent.
+         */
+        // super.addBehaviour(new
+        // Initialise("Sniffer","jade.tools.sniffer.Sniffer",new Object[]{ "*"
+        // }));
 
-		/*
-		 * Deploy Car agents.
-		 */
+        /*
+         * Deploy Car agents.
+         */
 
-		Integer carCounter = new Integer(0);
-		Integer carAmount = new Integer(2);
-		for (carCounter = 0; carCounter < carAmount; carCounter++) {
-			super.addBehaviour(new Initialise("CarAgent" + carCounter.toString(),"agents.CarAgent", null));
-		}
-		
+        Integer carCounter = new Integer(0);
+        Integer carAmount = new Integer(2);
+        for (carCounter = 0; carCounter < carAmount; carCounter++) {
+            super.addBehaviour(new Initialise("CarAgent" + carCounter.toString(),
+                    "agents.CarAgent", null));
+        }
+
         /*
          * Deploy Summary agents.
          */
@@ -35,8 +39,20 @@ public class Initialiser extends Agent {
         Integer summaryCounter = new Integer(0);
         Integer summaryAmount = new Integer(1);
         for (summaryCounter = 0; summaryCounter < summaryAmount; summaryCounter++) {
-            super.addBehaviour(new Initialise("SummaryAgent" + summaryCounter.toString(),"agents.SummaryAgent", null));
+            super.addBehaviour(new Initialise("SummaryAgent" + summaryCounter.toString(),
+                    "agents.SummaryAgent", null));
         }
-	}
+
+        /*
+         * Deploy Summary agents.
+         */
+
+        Integer transformerCounter = new Integer(0);
+        Integer transformerAmount = new Integer(1);
+        for (transformerCounter = 0; transformerCounter < transformerAmount; transformerCounter++) {
+            super.addBehaviour(new Initialise("TransformerAgent" + transformerCounter.toString(),
+                    "agents.TransformerAgent", null));
+        }
+    }
 
 }
